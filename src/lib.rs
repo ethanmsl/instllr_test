e5620da254eb634009958c4d51b830e967ee67cc
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-18 14:48:39 -0800

    explore: test structure of os comand

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
 //! lib.rs
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::io;
+use std::process::{Command, Output};
+
+pub fn echo_cmd(to_say: &str) -> io::Result<Output> {
+    let output = Command::new("echo").arg(to_say).output()?;
+
+    // assert_eq!(to_say.as_bytes(), output.stdout.as_slice());
+    Ok(output)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[test]
+//     fn it_works() {
+//         let result = add(2, 2);
+//         assert_eq!(result, 4);
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 //! main.rs
 //!
 
+use instllr_tst::echo_cmd;
 fn main() {
     println!("Hello, world! :)");
+    let out = echo_cmd("Hello world -- from Rust via terminal Command!!! :P :P")
+        .expect("Failed to execute command");
+    println!("Output: {}", String::from_utf8_lossy(&out.stdout));
 }

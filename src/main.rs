994e299dcbad3e4fdf90ddb2329fc89f250b7314
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-29 20:27:24 -0800

    using `run_cmd` crate's `run_cmd!()`

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,49 @@
 //! main.rs
 //!
 
-use instllr_tst::*;
-fn main() {
-    println!("-------------");
-    println!("Hello, world! :)");
+// use instllr_tst::*;
+// use std::process::Command;
+use cmd_lib::run_cmd;
 
-    println!("-------------");
-    let out = echo_cmd("Hello world -⦳ℯ- from Rust via terminal Command!!! :P :P")
-        .expect("Failed to execute command");
-    println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-    println!("Output: {:?}", &out.stdout);
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let msg = "I love rust";
+    run_cmd!(echo $msg)?;
+    run_cmd!(echo "This is the message: $msg")?;
+    run_cmd!(brew install procs)?;
+    Ok(())
 
-    println!("-------------");
-    let out = pwd_cmd()
-        .expect("Failed to execute command");
-    println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    // println!("-------------");
+    // println!("Hello, world! :)");
+    //
+    // println!("-------------");
+    // let out = echo_cmd("Hello world -⦳ℯ- from Rust via terminal Command!!! :P :P")
+    //     .expect("Failed to execute command");
+    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    // println!("Output: {:?}", &out.stdout);
+    //
+    // println!("-------------");
+    // let out = pwd_cmd().expect("Failed to execute command");
+    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    //
+    // // println!("-------------");
+    // // let out = up_cmd().expect("Failed to execute command");
+    // // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    //
+    // println!("-------------");
+    // let out = Command::new("cd");
+    // out.arg("src")
+    //     .arg("ls")
+    //     .output()
+    //     .expect("Failed to execute command");
+    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    //
+    // println!("-------------");
+    // let out = pwd_cmd().expect("Failed to execute command");
+    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
 
-    println!("-------------");
-    let out = up_cmd()
-        .expect("Failed to execute command");
-    println!("Output: {}", String::from_utf8_lossy(&out.stdout));
+    // println!("-------------");
+    // let out = Command::new("brew").arg("uninstall").arg("procs")
+    //     .output()
+    //     .expect("Failed to execute command");
+    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
 }

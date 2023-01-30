c4e8816aa12da2aad8a3f79b441f5923cf8e6763
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-01-29 21:49:37 -0800

    proto-feat: `proto_install_script()` runs the main repo installs
    Needs robustness and appropriate retry/skip logic.
    (in particular: `xcode-select --install` errors if already installed,
    unlike with the `brew` install commands)

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,30 @@
 //! lib.rs
 
+use cmd_lib::*;
 use std::io;
 use std::process::{Command, Output};
 
-/// does not echo to terminal that cargo runs it from, but output is printable 
+pub fn proto_install_script() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Note: `xcode-select --install` will **error** if already installed
+    let output = run_cmd!(xcode-select --install);
+    println!("result of running 'xcode-select --install': {:?}", output);
+
+    let output = run_cmd! {
+        brew tap wez/wezterm;
+        brew install --cask wez/wezterm/wezterm;
+        brew install starship zsh-autosuggestions zsh-syntax-highlighting bat choose-rust git-delta exa fd fzf navi ripgrep sd tealdeer viu zoxide eth-p/software/bat-extras-batman;
+        brew tap homebrew/cask-fonts;
+        brew install --cask font-anonymous-pro font-hack font-iosevka font-iosevka-slab font-major-mono-display font-syne-mono font-victor-mono;
+    }?;
+    println!(
+        "result of running `baby_install()`: {:?}",
+        output
+    );
+    Ok(())
+}
+
+/// does not echo to terminal that cargo runs it from, but output is printable
 /// after conversion from utf8
 pub fn echo_cmd(to_say: &str) -> io::Result<Output> {
     let output = Command::new("echo").arg(to_say).output()?;
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,42 @@
 //! main.rs
 //!
 
-// use instllr_tst::*;
-// use std::process::Command;
-use cmd_lib::run_cmd;
+use instllr_tst::*;
+use cmd_lib::{run_cmd, run_fun};
+use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    ///////// cmd_lib //////////
+    proto_install_script()?;
+
+    ///////// cmd_lib //////////
     let msg = "I love rust";
     run_cmd!(echo $msg)?;
     run_cmd!(echo "This is the message: $msg")?;
-    run_cmd!(brew install procs)?;
+    let res1 = run_cmd!(brew install procs);
+    println!(
+        "result of running 'brew install procs' using `run_cmd!`: {:?}",
+        res1
+    );
+
+    let res2 = run_fun!(brew install procs);
+    println!(
+        "result of running 'brew install procs' using `run_fun!`: {:?}",
+        res2
+    );
+
+    ///////// std::process //////////
+    let output = Command::new("brew").arg("install").arg("procs").output()?;
+    println!(
+        "result of running 'brew install procs' using std::process: {:?}",
+        String::from_utf8_lossy(&output.stdout)
+    );
+
+    Command::new("echo")
+        .arg("hi y'all")
+        .status()
+        .unwrap();
+
+    ///////// OK //////////
     Ok(())
-
-    // println!("-------------");
-    // println!("Hello, world! :)");
-    //
-    // println!("-------------");
-    // let out = echo_cmd("Hello world -⦳ℯ- from Rust via terminal Command!!! :P :P")
-    //     .expect("Failed to execute command");
-    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-    // println!("Output: {:?}", &out.stdout);
-    //
-    // println!("-------------");
-    // let out = pwd_cmd().expect("Failed to execute command");
-    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-    //
-    // // println!("-------------");
-    // // let out = up_cmd().expect("Failed to execute command");
-    // // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-    //
-    // println!("-------------");
-    // let out = Command::new("cd");
-    // out.arg("src")
-    //     .arg("ls")
-    //     .output()
-    //     .expect("Failed to execute command");
-    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-    //
-    // println!("-------------");
-    // let out = pwd_cmd().expect("Failed to execute command");
-    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
-
-    // println!("-------------");
-    // let out = Command::new("brew").arg("uninstall").arg("procs")
-    //     .output()
-    //     .expect("Failed to execute command");
-    // println!("Output: {}", String::from_utf8_lossy(&out.stdout));
 }

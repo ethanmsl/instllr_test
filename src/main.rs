98bffa12535a11578680474c6ae865d0b74e33f9
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-01 23:38:49 -0800

    feats: home_dir determination & {status, stdout, stderr} output

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,80 @@
 //! main.rs
 //!
 
-use instllr_tst::*;
 use cmd_lib::{run_cmd, run_fun};
+use dirs::home_dir;
+use instllr_tst::*;
 use std::process::Command;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    ///////// cmd_lib //////////
-    proto_install_script()?;
+    // Command::new("ls")
+    //     .arg("-l")
+    //     .arg("-a")
+    //     .spawn()
+    //     .expect("ls command failed to start");
+    println!("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-    ///////// cmd_lib //////////
-    let msg = "I love rust";
-    run_cmd!(echo $msg)?;
-    run_cmd!(echo "This is the message: $msg")?;
-    let res1 = run_cmd!(brew install procs);
-    println!(
-        "result of running 'brew install procs' using `run_cmd!`: {:?}",
-        res1
-    );
+    let home_dir = home_dir().expect("Home Directory ('~') not found");
+    println!("home_dir: {:?}", home_dir);
 
-    let res2 = run_fun!(brew install procs);
-    println!(
-        "result of running 'brew install procs' using `run_fun!`: {:?}",
-        res2
-    );
+    Command::new("exa")
+        .current_dir(home_dir)
+        .spawn()
+        .expect("ls command failed to start");
 
-    ///////// std::process //////////
-    let output = Command::new("brew").arg("install").arg("procs").output()?;
-    println!(
-        "result of running 'brew install procs' using std::process: {:?}",
-        String::from_utf8_lossy(&output.stdout)
-    );
-
-    Command::new("echo")
-        .arg("hi y'all")
+    let mut xcode = Command::new("xcode-select");
+    xcode.arg("--version");
+    xcode.spawn().expect("xcode-select command failed to start");
+    xcode
         .status()
-        .unwrap();
+        .expect("xcode-select command failed to start");
+    let out = xcode
+        .output()
+        .expect("xcode-select command failed to start");
+    xcode.spawn().expect("xcode-select command failed to start");
+    println!("~~~~~~~~~~~~~~~~~~~");
+    println!("output is: {:?}", out);
+
+    println!("~~~~~~~~~~~~~~~~~~~");
+    println!("~~~~~~~~~~~~~~~~~~~");
+    println!("~~~~~~~~~~~~~~~~~~~");
+
+    ///////// cmd_lib //////////
+    // proto_install_script()?;
+    // let output = run_cmd!(xcode -select --version);
+    // println!("result of running 'xcode-select --version': {:?}", output);
+    // println!();
+    //
+    // let output = Command::new("xcode-select").arg("--version").output()?;
+    // println!(
+    //     "result of running 'xcode-select --version' using std::process: {:?}",
+    //     &output.stderr
+    // );
+
+    ///////// cmd_lib //////////
+    // let msg = "I love rust";
+    // run_cmd!(echo $msg)?;
+    // run_cmd!(echo "This is the message: $msg")?;
+    // let res1 = run_cmd!(brew install procs);
+    // println!(
+    //     "result of running 'brew install procs' using `run_cmd!`: {:?}",
+    //     res1
+    // );
+    //
+    // let res2 = run_fun!(brew install procs);
+    // println!(
+    //     "result of running 'brew install procs' using `run_fun!`: {:?}",
+    //     res2
+    // );
+    //
+    // ///////// std::process //////////
+    // let output = Command::new("brew").arg("install").arg("procs").output()?;
+    // println!(
+    //     "result of running 'brew install procs' using std::process: {:?}",
+    //     String::from_utf8_lossy(&output.stdout)
+    // );
+    //
+    // Command::new("echo").arg("hi y'all").status().unwrap();
 
     ///////// OK //////////
     Ok(())

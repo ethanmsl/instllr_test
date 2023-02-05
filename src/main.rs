c03b2542b046c2c5a0b543c991295d1a751ce296
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-05 14:24:49 -0800

    explor(discov): piping in with xargs

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ use cmd_lib::*;
 use instllr_tst::check_installation::is_in_path;
 use instllr_tst::install_loop::install_loop;
 use instllr_tst::RunnerInfo;
+// use std::error;
+use std::io;
+use std::process::{Command, Stdio};
+// use std::process::ChildStdin;
+use std::io::Write;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let host_info = RunnerInfo::new();
@@ -18,13 +23,24 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     //     Some(status) => println!("status: {:?}", status),
     //     None => println!("status: None"),
     // }
+    set_inp("use".to_string())?;
+
+    // let mut child = Command::new("rev")
+    //     .stdin(Stdio::piped())
+    //     .stdout(Stdio::piped())
+    //     .spawn()
+    //     .expect("Failed to spawn child process");
+    //
+    // let mut stdin = child.stdin.take().expect("Failed to open stdin");
+    // std::thread::spawn(move || {
+    //     stdin
+    //         .write_all("Hello, world!".as_bytes())
+    //         .expect("Failed to write to stdin");
+    // });
+
+    //////////////////////////
     println!();
 
-    run_fun! {
-        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    }?;
-
-    println!();
     let to_echo = vec!["sk", "broot", "bat"];
     // let to_do = r#"echo \"oh my gosh did this work?!?!\""#;
     for lword in to_echo {
@@ -35,7 +51,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("success_state?: {:?}", is_in_path(lword)?);
     }
 
-    println!("which sk is: {}", run_fun!{which sk}?);
+    println!("which sk is: {}", run_fun! {which sk}?);
+    // println!("xcode res is: {}", run_fun!{xcode-select --install}?);
 
     println!("\n----------------------\n");
 
@@ -53,14 +70,41 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // xcode
 
-    {
-        let xcs = is_in_path("xcode-select")?;
-        println!("xcs: {}", xcs);
-    }
+    let xcs = is_in_path("xcode-select")?;
+    println!("xcs: {}", xcs);
 
     // brew
 
     // sub-brews
 
     Ok(())
+}
+// match Command::new("/bin/bash")
+//     .stdin(Stdio::piped())
+//     .arg("-c")
+//     .arg(r#"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"#)
+//     .spawn()
+
+fn set_inp(inp: String) -> io::Result<()> {
+    match Command::new("xargs")
+        .stdin(Stdio::piped())
+        .arg("rg")
+        .spawn()
+    {
+        Ok(mut child) => {
+            child
+                .stdin
+                .as_ref()
+                .unwrap()
+                .write_all(inp.as_bytes())
+                .unwrap();
+
+            child.wait().unwrap();
+            Ok(())
+        }
+        Err(_e) => Err(io::Error::new(
+            io::ErrorKind::Other,
+            "failed to execute process",
+        )),
+    }
 }
